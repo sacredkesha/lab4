3f925ae8120ad38d3ae579ce26a24d63edf182cc
sacredkesha <107679920+sacredkesha@users.noreply.github.com>
2022-12-24 10:56:23 +0200

    Add files via upload

--- 2.py
+++ 2.py
@@ -13,32 +13,46 @@ bucket = 'sacredbucket1'
 s3_resource = boto3.resource('s3')
 s3_client = boto3.client('s3')
 
+def download_files():
+    for i in months:
+        link = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=2021{i}01&json'
+        resp = requests.get(url=link)
+        data = resp.json()
+        pd.json_normalize(data).to_csv(f'exchange_{i}.csv')
 
-for i in months:
-    link = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date=2021{i}01&json'
-    resp = requests.get(url=link)
-    data = resp.json()
-    pd.json_normalize(data).to_csv(f'exchange_{i}.csv')
-    s3_resource.meta.client.upload_file(f'exchange_{i}.csv', bucket, f'exchange_{i}.csv')
+def upload_to_bucket():
+    for i in months:
+        s3_resource.meta.client.upload_file(f'exchange_{i}.csv', bucket, f'exchange_{i}.csv')
 
-for i in months:
-    obj = s3_client.get_object(Bucket=bucket, Key=f'exchange_{i}.csv')
-    df = pd.read_csv(obj['Body'])
-    dfs.append(df) 
-res=pd.concat(dfs)
+def read_from_bucket():
+    for i in months:
+        obj = s3_client.get_object(Bucket=bucket, Key=f'exchange_{i}.csv')
+        df = pd.read_csv(obj['Body'])
+        dfs.append(df) 
+    res=pd.concat(dfs)
 
-usd = res[res['cc'] == 'USD']
-eur = res[res['cc'] == 'EUR']
+    usd = res[res['cc'] == 'USD']
+    eur = res[res['cc'] == 'EUR']
 
-x_usd =[i for i in months]
-x_eur =[i for i in months]
-y_usd = np.array(usd['rate'])
-y_eur = np.array(eur['rate'])
-plt.plot(x_usd,y_usd,label='usd')
-plt.plot(x_eur,y_eur,label='eur')
-plt.title('UAH to ')
-plt.legend()
-plt.show
-plt.savefig('1.png')
+    x_usd =[i for i in months]
+    x_eur =[i for i in months]
+    y_usd = np.array(usd['rate'])
+    y_eur = np.array(eur['rate'])
+    plt.plot(x_usd,y_usd,label='usd')
+    plt.plot(x_eur,y_eur,label='eur')
+    plt.title('UAH to ')
+    plt.legend()
+    plt.show
+    plt.savefig('1.png')
 
-s3_resource.meta.client.upload_file('1.png', bucket, '1.png')
+    s3_resource.meta.client.upload_file('1.png', bucket, '1.png')
+
+    
+def main():
+    download_files()
+    upload_to_bucket()
+    read_from_bucket()
+
+
+if __name__ == "__main__":
+    main()    
